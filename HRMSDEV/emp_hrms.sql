72f59ffd21a9f937a870f16d518331c38d6d11b3
DevShadiq <shadiq1297@gmail.com>
2025-03-18 15:39:39 +0600

    Update March 18

---
+++ HRMSDEV/emp_hrms.sql
@@ -0,0 +1,60 @@
+SELECT ID,
+       EMP_ID,
+       EMPNO,
+       NID,
+       F_NAME,
+       L_NAME,
+       DOB,
+       MOBILE,
+       JOIN_DATE,
+       EMAIL,
+       GENDER,
+MARITAL_STATUS,
+       FATHER,
+       MOTHER,       
+       HEIGHT,
+       WEIGHT,
+       BLOOD,
+       religion,
+       ADDRESS,
+       P_ADDRESS, 
+       PASSW,
+
+
+       DEPT_ID,
+       LOC_ID,
+
+
+       COM_ID,
+
+       C_POST,
+       C_THANA,
+       C_DISTRICT,
+       
+       --       C_DIVISION,
+       
+       P_POST,
+       P_THANA,
+       P_DISTRICT,
+
+
+       --P_DIVISION,
+       
+       
+       EMP_TYPE,
+       
+       BRANCH_ID,
+       DESIG_ID,
+       
+       JOB_ID,
+       USER_GRP,
+       
+       ENT_BY,
+       ENT_DATE,
+       UPD_BY,
+       UPD_DATE,
+       STATUS,
+       MIME_TYPE,
+       FILE_NAME,
+       EMPNO
+  FROM employees;
---
+++ HRMSDEV/emp_ipihr_hrms.sql
@@ -0,0 +1,41 @@
+/* Formatted on 3/17/2025 10:10:37 AM (QP5 v5.362) */
+SELECT id,
+       emp_id,
+       empno,
+       nid,
+       first_name F_NAME,
+       last_name L_NAME,
+       birthdate dob,
+       phone,       
+       join_date,       
+       email,
+       empsex,
+       maritalst MARITAL_STATUS,
+       father_name FATHER,
+       mother_name MOTHER,
+       height,
+       weight,
+       bld_group BLOOD,
+       religion,
+       add1 ADDRESS,
+       p_add1 P_ADDRESS,
+       '123' PASSW,
+
+
+       department_name,
+       location,
+       businessunit,
+       businessunitid,
+       ccode,
+
+
+       
+       add2,
+       add3,
+       add4,
+       
+       p_add2,
+       p_add3,
+       p_add4
+  FROM v_emp_hrms;
+

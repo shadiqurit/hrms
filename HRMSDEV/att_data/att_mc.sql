6ab879662760251d645446bd030a1f465299abc8
Shadiq <shadiq1297@gmail.com>
2025-09-08 14:42:14 +0600

    Report update at monthly leave report

--- HRMSDEV/att_data/att_mc.sql
+++ HRMSDEV/att_data/att_mc.sql
@@ -1,28 +1,34 @@
-CREATE OR REPLACE PROCEDURE attance_sync_apidb
-AS
-BEGIN
-    -- 1. Insert all data in one go using INSERT SELECT
-    INSERT INTO ATT_MCDATA@apihr (
+CREATE
+OR REPLACE PROCEDURE attance_sync_apidb AS BEGIN -- 1. Insert all data in one go using INSERT SELECT
+INSERT INTO
+    ATT_MCDATA @apihr (
         ID,
         USERID,
         ATTTIME,
         DEVICE_ID,
         STATUS
     )
-    SELECT 
-        att_mcdata_seq.NEXTVAL@apihr,
-        userid,
-        att_time,
-        device,
-        status
-    FROM attendance_time
-    WHERE NVL(flag, 'N') = 'N';
+SELECT
+    att_mcdata_seq.NEXTVAL @apihr,
+    userid,
+    att_time,
+    device,
+    status
+FROM
+    attendance_time
+WHERE
+    NVL(flag, 'N') = 'N';
 
-    -- 2. Update flag in bulk  
-    UPDATE attendance_time
-    SET flag = 'Y'
-    WHERE NVL(flag, 'N') = 'N';
+-- 2. Update flag in bulk  
+UPDATE
+    attendance_time
+SET
+    flag = 'Y'
+WHERE
+    NVL(flag, 'N') = 'N';
 
-    COMMIT;
+COMMIT;
+
 END;
+
 /
